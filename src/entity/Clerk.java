58dee97704999deb2efd422975a967972b1fb419
ymcvalu <ymccmy66@163.com>
2017-05-04 22:30:46 +0800

    Initial commit

--- src/entity/Clerk.java
+++ src/entity/Clerk.java
@@ -4,13 +4,115 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.Date;
 
 /**
  * Created by ymcvalu on 2017/5/4.
  */
+
+
 @Entity
 public class Clerk {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     int id;
+    String identity;
+    String name;
+    String phone;
+    String address;
+    Date birthday;
+    Date entry_time;
+    String weichat;
+    String salary_card;
+    int status;
+    String duties;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getIdentity() {
+        return identity;
+    }
+
+    public void setIdentity(String identity) {
+        this.identity = identity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public Date getEntry_time() {
+        return entry_time;
+    }
+
+    public void setEntry_time(Date entry_time) {
+        this.entry_time = entry_time;
+    }
+
+    public String getWeichat() {
+        return weichat;
+    }
+
+    public void setWeichat(String weichat) {
+        this.weichat = weichat;
+    }
+
+    public String getSalary_card() {
+        return salary_card;
+    }
+
+    public void setSalary_card(String salary_card) {
+        this.salary_card = salary_card;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getDuties() {
+        return duties;
+    }
+
+    public void setDuties(String duties) {
+        this.duties = duties;
+    }
 }
