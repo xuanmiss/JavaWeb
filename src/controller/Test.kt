5861c85a2f88a384317e649be538227276870551
ymcvalu <ymccmy66@163.com>
2017-05-04 23:10:09 +0800

    Initial commit

--- src/controller/Test.kt
+++ src/controller/Test.kt
@@ -2,8 +2,11 @@ package controller
 
 import com.opensymphony.xwork2.Action
 import com.opensymphony.xwork2.ActionSupport
+import dao.IClerkDBAccessor
+import entity.Clerk
 import org.springframework.context.annotation.Scope
 import org.springframework.stereotype.Controller
+import javax.annotation.Resource
 
 /**
  * Created by ymcvalu on 2017/5/4.
@@ -11,8 +14,15 @@ import org.springframework.stereotype.Controller
 @Controller("test")
 @Scope("prototype")
 class Test:ActionSupport(){
-    val msg="a test page"
+    var count:Int=0
+    val msg="count of clerk:"
+    @Resource(name="clerkDBAcc")
+    lateinit var clerkDBAcc:IClerkDBAccessor
     override  fun execute():String{
+        var session=clerkDBAcc.getSession()
+        session.transaction.begin()
+        count=clerkDBAcc.getCount(Clerk::class.java)
+        session.transaction.commit()
         return Action.SUCCESS
     }
 }
--- web/test.jsp
+++ web/test.jsp
@@ -12,6 +12,6 @@
     <title>Title</title>
 </head>
 <body>
-    <h1><s:property value="msg"/></h1>
+    <h1><s:property value="msg"/><s:property value="count"/> </h1>
 </body>
 </html>
