362f36e93b7bbfd62113101e90276894106a74aa
ymcvalu <ymccmy66@163.com>
2017-05-04 22:20:08 +0800

    Initial commit

--- src/dao/ClerkDBAccessor.java
+++ src/dao/ClerkDBAccessor.java
@@ -1,6 +1,8 @@
 package dao;
 
 import entity.Clerk;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Repository;
 
 /**
  * Created by ymcvalu on 2017/5/4.
@@ -9,6 +11,8 @@ import entity.Clerk;
 /**
  * 员工表访问实现类
  */
+@Repository("clerkDBAcc")
+@Lazy
 public class ClerkDBAccessor extends BaseDBAccessor<Clerk> implements IClerkDBAccessor {
     /**
      * 实现新方法...
