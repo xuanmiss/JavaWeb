0fec4cb545494e080a213895690445b29adff247
xuanmiss <1511836114@qq.com>
2017-05-06 14:31:52 +0800

    miss 三个a

--- src/dao/AccountDBAccessor.java
+++ src/dao/AccountDBAccessor.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package dao;
 
 import entity.Clerk;
@@ -17,23 +17,4 @@ public class AccountDBAccessor {
          */
     }
 }
-=======
-package dao;
 
-import entity.Clerk;
-import org.springframework.context.annotation.Lazy;
-import org.springframework.stereotype.Repository;
-
-/**
- * Created by miss on 2017/5/6.
- */
-public class AccountDBAccessor {
-    @Repository("accountDBAcc")
-    @Lazy
-    public class ClerkDBAccessor extends BaseDBAccessor<Clerk> implements IClerkDBAccessor {
-        /**
-         * 在这里实现新方法...
-         */
-    }
-}
->>>>>>> origin/master
