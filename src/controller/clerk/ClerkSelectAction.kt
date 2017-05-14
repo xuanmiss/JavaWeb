5fb72feed62ce952a36de83a31d4f16345993188
DeleteIt <962922780@qq.com>
2017-05-14 21:50:07 +0800

    clerkAdd..clerkSelect

---
+++ src/controller/clerk/ClerkSelectAction.kt
@@ -0,0 +1,25 @@
+package controller.brand
+
+import com.opensymphony.xwork2.ActionSupport
+import entity.Clerk
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Scope
+import org.springframework.stereotype.Controller
+import service.clerk.IClerkHandleSvc
+import util.PageBean
+
+/**
+ * Created by ymcvalu on 2017/5/11.
+ */
+@Controller("selectClerk")
+@Scope("prototype")
+open class ClerkSelectAction:ActionSupport(){
+    @Autowired
+    private lateinit var clerkSvc: IClerkHandleSvc
+    open var pageBean:PageBean<Clerk>?=null
+    open var pageNo:Int=1
+    override fun execute(): String {
+        pageBean=clerkSvc.getListByPage(pageNo)
+        return "list"
+    }
+}
