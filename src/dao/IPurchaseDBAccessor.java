ef38a4bf283509729186284230e4fbd5607a6ac6
ymcvalu <ymccmy66@163.com>
2017-05-22 23:42:10 +0800

    order

---
+++ src/dao/IPurchaseDBAccessor.java
@@ -0,0 +1,16 @@
+package dao;
+
+import entity.Batch;
+import entity.Clerk;
+import entity.Order;
+
+import java.util.List;
+
+/**
+ * Created by ymcvalu on 2017/5/22.
+ */
+public interface IPurchaseDBAccessor {
+    List<Batch> getBatchList();
+    List<Clerk> getClerkList();
+    List<Order> getOrderList();
+}
